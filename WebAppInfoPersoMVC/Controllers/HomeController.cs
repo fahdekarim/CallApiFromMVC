05635bbbb3dd66c68badbe0905ef73b6a46e06af
fahdekarim <dahfmirak@gmail.com>
2016-12-18 04:08:36 +0100

    this is what im looking for , its a call from an cloud Sigfox

--- WebAppInfoPersoMVC/Controllers/HomeController.cs
+++ WebAppInfoPersoMVC/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
@@ -15,7 +16,8 @@ namespace WebAppInfoPersoMVC.Controllers
     {
         // GET: Home
         HttpClient client;
-        int Countoflist;
+        
+        string c = "https://backend.sigfox.com/api/devices/17AA23/messages";  
         //The URL of the WEB API Service
         string url = "http://localhost:59897/api/infoperso";
 
@@ -25,13 +27,16 @@ namespace WebAppInfoPersoMVC.Controllers
         public HomeController()
         {
             client = new HttpClient();
-            client.BaseAddress = new Uri(url);
+            client.BaseAddress = new Uri(c);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        }
+            var byteArray = Encoding.ASCII.GetBytes("5842fff59058c2525ddd85c2:8e8d25dc19fcc43bcc0b0432301fd5cc");
+            var header = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+            client.DefaultRequestHeaders.Authorization = header;
 
-        // GET: EmployeeInfo
-        public async Task<ActionResult> Index()
+
+        }
+        public async Task<ActionResult> index()
         {
             HttpResponseMessage responseMessage = await client.GetAsync(url);
             if (responseMessage.IsSuccessStatusCode)
@@ -39,8 +44,23 @@ namespace WebAppInfoPersoMVC.Controllers
                 var responseData = responseMessage.Content.ReadAsStringAsync().Result;
 
                 var Employees = JsonConvert.DeserializeObject<List<infoperso>>(responseData);
-               TempData["id"]= Employees.Count;
+                TempData["id"] = Employees.Count;
 
+                return View(Employees);
+            }
+            return View("Error");
+        }
+        // GET: EmployeeInfo
+        public async Task<ActionResult> Sigfox()
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(c);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+ 
+                var Employees = JsonConvert.DeserializeObject<IOTSigfoxData>(responseData);
+                //  TempData["id"]= Employees.Count;
+             
                 return View(Employees);
             }
             return View("Error");
---
+++ WebAppInfoPersoMVC/Models/IOTSigfoxData.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppInfoPersoMVC.Models
+{
+    public class IOTSigfoxData
+    {
+        [JsonProperty(PropertyName = "data")]
+        List<IOTSigfox> data { get; set; }
+       
+    }
+}
